30b79b2921118190f900a1deddc4a8d75f0fd8d4
wizard <r13ayan@gmail.com>
2025-01-21 17:17:45 +0600

    append

--- learndjango/api/views.py
+++ learndjango/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Books
 
-# Create your views here.
+
+class BooksListCreate(generics.ListCreateAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+    
+    
+    
+
